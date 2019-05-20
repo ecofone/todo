475ad4814c116b3037675957572c00ca3e649b82
Enrique Cofone <enrique.cofone@gmail.com>
2019-05-20 19:23:55 -0300

    added build folder

---
+++ client/build/precache-manifest.5ee962bbd36cd8bb743072a01dbefa14.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13ad107d41d3c43d9e788daf723c6e75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9217b8bd443b6cabd568",
+    "url": "/static/js/2.68615c39.chunk.js"
+  },
+  {
+    "revision": "e23feaee53570bc57622",
+    "url": "/static/js/main.37abcac4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
